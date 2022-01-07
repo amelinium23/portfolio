0c039d32b2cc2dfc91254bba817a0268c583db6b
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-01-07 17:10:08 +0100

    cleanup

--- portfolio-app/src/components/App.tsx
+++ portfolio-app/src/components/App.tsx
@@ -1,10 +1,7 @@
-import "../css/App.css"
 import AppWraper from "./AppWraper"
 import Projects from "./Projects";
-import {
-  Route,
-  Routes
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import "../css/App.css"
 
 
 export default function App() {
--- portfolio-app/src/components/ContactMe.tsx
+++ portfolio-app/src/components/ContactMe.tsx
@@ -21,10 +21,18 @@ export default function ContactMe() {
             </Container>
             <Container style={sectionStyle}>
                 <p>You can contact me via: </p>
-                <a href="https://github.com/amelinium23" target={"blank"}><Github style={iconStyle} height={70} width={70} /></a>
-                <a href="https://www.instagram.com/amelinium23/" target={"blank"}><Instagram style={iconStyle} height={70} width={70} /></a>
-                <a href="https://www.linkedin.com/in/piotr-tomczak-565666209/" target={"blank"}><Linkedin style={iconStyle} height={70} width={70} /></a>
-                <a href="mailto:piotr.tomczak.praca@gmail.com"><Envelope style={iconStyle} height={70} width={70} /></a>
+                <a href="https://github.com/amelinium23" target={"blank"}>
+                    <Github style={iconStyle} height={70} width={70} />
+                </a>
+                <a href="https://www.instagram.com/amelinium23/" target={"blank"}>
+                    <Instagram style={iconStyle} height={70} width={70} />
+                </a>
+                <a href="https://www.linkedin.com/in/piotr-tomczak-565666209/" target={"blank"}>
+                    <Linkedin style={iconStyle} height={70} width={70} />
+                </a>
+                <a href="mailto:piotr.tomczak.praca@gmail.com">
+                    <Envelope style={iconStyle} height={70} width={70} />
+                </a>
             </Container>
         </Container>
     )
